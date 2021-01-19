a9e0471adf4acd357f8ff35547b8f45577de2415
Nana Lau <nanalau315@hotmail.com>
2021-01-18 20:12:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here!
+// // Write your code here!
+// let element = document.createElement('div')
+
+// document.body.appendChild(element)
+
+// let ul = document.createElement('ul')
+
+// for (let i =0; i < 3; i++){
+//     let li = document.createElement('li')
+//     li.innerHTML = (i + 1).toString()
+//     ul.appendChild(li)
+// }
+// element.appendChild(ul)
+
+// let element = document.querySelector("p#greeting");
+// element.innerHTML = 'Hello, DOM!' 
+
+// element.style.backgroundColor = '#27647B'; 
+
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left' 
+
+let main = document.querySelector('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = `Nana is the champion`
+body.append(newHeader)
